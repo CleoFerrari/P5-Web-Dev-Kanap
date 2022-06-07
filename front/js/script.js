8bd5b9b22d42a754babd76a3a397791c7e0c1045
Cleo Ferrari <cleonice.ferrari@hotmail.com>
2022-06-07 14:54:15 +0100

    project 5

---
+++ front/js/script.js
@@ -0,0 +1,24 @@
+const apiUrl = `http://localhost:3000/api/products`;
+
+const items = document.getElementById(`items`)
+
+const getData = async () => {
+    try {
+
+        const response = await fetch(apiUrl);
+        const jsonResponse = await response.json();
+
+        jsonResponse.forEch(product => {
+            items.innerHTML += `
+            <a href="./product.html?id=${product._id}">
+            <article>
+              <img src="${product.imageUrl}" alt="${product.altText}">            
+              <h3 class="productName">${product.name}</h3>
+              <p class="productDescription">${product.description}</p>  
+            </article>
+            </a>`;
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
